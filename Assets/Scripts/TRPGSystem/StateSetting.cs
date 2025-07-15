0380ca6ac4da8b7ac4de79fd74d238bd5e15aaa2
dpa32 <128779850+dpa32@users.noreply.github.com>
2025-07-15 11:38:12 +0900

    .

---
+++ Assets/Scripts/TRPGSystem/StateSetting.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class StateSetting : MonoBehaviour
+{
+    private int remains = 500;
+
+
+    private void MinusRemains(int count)
+    {
+
+    }
+
+    private void PlusRemins(int count)
+    {
+
+    }
+}
